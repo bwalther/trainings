171967f9f960a9d040942993c855caa762250df0
Benjamin Walther <bwaltherb@gmail.com>
2022-05-14 12:03:45 +0200

    new problems

---
+++ coursera_algorithms1/src/main/java/Testing.java
@@ -0,0 +1,8 @@
+import edu.princeton.cs.algs4.IndexMinPQ;
+
+public class Testing {
+
+    public static void main(String[] args) {
+        IndexMinPQ.main(null);
+    }
+}
--- problems/src/main/java/arrays/TopKFrequent.java
+++ problems/src/main/java/arrays/TopKFrequent.java
@@ -3,6 +3,46 @@ package arrays;
 import java.util.*;
 
 public class TopKFrequent {
+
+    public static void main(String[] args) {
+        // ["KthLargest","add","add","add","add","add"]
+        //[[3,[4,5,8,2]],[3],[5],[10],[9],[4]]
+        KthLargest kthLargest = new KthLargest(3, new int[]{4,5,8,2});
+        int res = kthLargest.add(3);
+        res = kthLargest.add(5);
+        res = kthLargest.add(10);
+        res = kthLargest.add(9);
+        res = kthLargest.add(4);
+    }
+
+    // https://leetcode.com/problems/kth-largest-element-in-a-stream/
+    static class KthLargest {
+        PriorityQueue<Integer> pq;
+        int k;
+
+        public KthLargest(int k, int[] nums) {
+            HashMap m;
+
+            this.pq = new PriorityQueue<>(k);
+            this.k  = k;
+            for(int i: nums){
+                pq.add(i);
+                if (pq.size()>k){
+                    pq.poll();
+                }
+            }
+        }
+
+        public int add(int val) {
+            pq.add(val);
+            if (pq.size()>this.k){
+                pq.poll();
+            }
+            return pq.peek();
+        }
+    }
+
+    //https://leetcode.com/problems/top-k-frequent-elements/
     public int[] topKFrequent(int[] nums, int k) {
 
         int[] res = new int[k];
